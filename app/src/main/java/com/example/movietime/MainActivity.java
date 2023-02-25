3b1154e346720034f3bf1b7ea2dfa5551294e269
parneet-guraya <gurayaparneet@gmail.com>
2023-02-25 21:05:43 +0530

    Setup recyclerview to show the movie list

--- app/src/main/java/com/example/movietime/MainActivity.java
+++ app/src/main/java/com/example/movietime/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.movietime;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import com.example.movietime.adapter.MovieAdapter;
+import com.example.movietime.databinding.ActivityMainBinding;
+import com.example.movietime.glide.GlideImageLoader;
+import com.example.movietime.model.DataSource;
+import com.example.movietime.model.Movie;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+private ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+
+        List<Movie> movieList = new DataSource(getApplicationContext()).loadListData().getMovieList();
+        MovieAdapter adapter = new MovieAdapter(this,movieList,new GlideImageLoader(this));
+
+
+        mainBinding.movieListRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        mainBinding.movieListRecyclerView.setAdapter(adapter);
+
+
     }
 }
