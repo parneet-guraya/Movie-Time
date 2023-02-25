0f5ddbc65c0f81f93c1d0bfcbdf867c388f72771
parneet-guraya <gurayaparneet@gmail.com>
2023-02-25 16:35:24 +0530

    define glide loading logic

--- app/src/main/java/com/example/movietime/glide/GlideImageLoader.java
+++ app/src/main/java/com/example/movietime/glide/GlideImageLoader.java
@@ -3,24 +3,29 @@ package com.example.movietime.glide;
 import android.content.Context;
 import android.net.Uri;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.RequestManager;
 import com.example.movietime.interactor.ImageLoadingLibrary;
 
 public class GlideImageLoader implements ImageLoadingLibrary {
-private Context context;
+    private final Context context;
 
-    public GlideImageLoader(Context context){
+    public GlideImageLoader(Context context) {
         this.context = context;
     }
 
     // TODO: see is it okay to use this class name as return type?
     @Override
     public ImageLoadingLibrary init() {
-        return null;
+
+        return this;
     }
 
     @Override
     public void load(Uri uri, View imageView) {
-
+        Glide.with(context).load(uri).into((ImageView)imageView);
     }
 }
